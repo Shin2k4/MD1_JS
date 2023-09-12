37a53d18a4a8ffe650495ef556b45c93ad92d6f8
Shin2k4 <130373533+Shin2k4@users.noreply.github.com>
2023-09-13 00:03:16 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+
+// Bài 1:
+let yourAge = prompt("nhập tuổi của bạn");
+document.write(yourAge)
+
+// Bài 2:
+let a = "Wellcome to Rikkei Academy "
+console.log(a)
+
+// Bài 3:
+let c = "Quyết tâm học Javascript"
+console.log(c)
+
+// Bài 4:
+alert("Chào mừng bạn đến với học viện Rikkei Academy")
+
+// Bài 5:
+confirm("Bạn đã đủ 18 tuổi chưa!")
